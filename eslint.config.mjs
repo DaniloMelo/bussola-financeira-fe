570842dfa829cce798d80ff1e002a822e5229284
DaniloMelo <danilomarquesdemelo@gmail.com>
2025-11-01 13:22:39 -0300

    chore(eslint): setup ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,31 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tseslint from "typescript-eslint";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/require-await": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "*.config.mjs",
+    "*.config.js",
   ]),
 ]);
 
