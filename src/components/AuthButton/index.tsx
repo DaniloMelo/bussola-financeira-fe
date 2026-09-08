b48e3e9b6f8a8a4ab406548dec86ca425b80e529
DaniloMelo <danilomarquesdemelo@gmail.com>
2026-09-08 11:46:44 -0300

    fix: change AuthButton height

--- src/components/AuthButton/index.tsx
+++ src/components/AuthButton/index.tsx
@@ -9,8 +9,8 @@ export default function AuthButton({ text, ...props }: AuthButtonProps) {
     <button
       {...props}
       className={`
-        ${props.disabled ? "bg-zinc-500 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-800 cursor-pointer transition"}
-         text-white rounded-sm mt-4 p-1 font-semibold
+        ${props.disabled ? "bg-zinc-500 cursor-not-allowed" : "bg-blue-600 hover:bg-blue-800 cursor-pointer transition"}
+         text-white rounded-sm mt-4 p-1 font-semibold h-12 text-xl lg:h-8 lg:text-base
       `}
     >
       {props.disabled ? (
